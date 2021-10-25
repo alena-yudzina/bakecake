7683f17d7b78167ae105bcf9d757769158610045
stas <garanzha.s.a@gmail.com>
2021-10-25 23:05:48 +0300

    change timezone and language

--- bakecake/settings.py
+++ bakecake/settings.py
@@ -79,13 +79,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LANGUAGE_CODE = 'ru-RU'
 
-# Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
