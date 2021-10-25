d4334ed2692e4c93f1a5ebc769719886a611c239
stas <garanzha.s.a@gmail.com>
2021-10-25 23:05:14 +0300

    refactor settings.py

--- bakecake/settings.py
+++ bakecake/settings.py
@@ -23,7 +23,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop.apps.ShopConfig', 
+    # local_apps
+    'shop.apps.ShopConfig',
 ]
 
 MIDDLEWARE = [
@@ -56,20 +57,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'bakecake.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
