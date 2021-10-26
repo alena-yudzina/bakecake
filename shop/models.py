407684c325202935f22fbc7162a816c8242b0051
Stas Garanzha <garanzha.s.a@yandex.ru>
2021-10-26 13:47:23 +0300

    start creating cake models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.base import Model
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class CakeLevel(models.Model):
+    level = models.PositiveSmallIntegerField()
+    price = models.PositiveSmallIntegerField()
+
+
+class CakeForm(models.Model):
+    type = models.CharField(max_length=100)
+    price = models.PositiveSmallIntegerField()
+
+
+class Topping(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.PositiveSmallIntegerField()
+
+
+class Berry(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.PositiveSmallIntegerField()
+
+
+class Decor(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.PositiveSmallIntegerField()
+
+
+class Cake(models.Model):
+    # Вопрос - должны ли оставаться в базе заказы клиента при удалении аккаунта?
+    client = models.ForeignKey(to=User,
+                               on_delete=models.SET_NULL,
+                               null=True,
+                               verbose_name='клиент')
+    level = models.ForeignKey(to=CakeLevel,
+                              on_delete=models.SET_NULL,
+                              null=True,
+                              verbose_name='уровень')
+    topping = models.ManyToManyField(to=Topping,
+                                     verbose_name='топпинги')
+    berry = models.ManyToManyField(to=Berry,
+                                   verbose_name='ягоды')
+    decor = models.ManyToManyField(to=Decor,
+                                   verbose_name='декор')
+    caption_on_cake = models.CharField(blank=True,
+                                       verbose_name='надпись на торте')
+
+
+class Order(models.Model):
+    # Вопрос - один заказ - один торт?
+    cake = models.ForeignKey(to=Cake,
+                             on_delete=models.SET_NULL,
+                             null=True,
+                             verbose_name='торт')
