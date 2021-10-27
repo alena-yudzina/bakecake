b68b94e3722c8f020b6644c8bd258f7e11464f5f
Stas Garanzha <garanzha.s.a@yandex.ru>
2021-10-28 02:19:30 +0300

    urlpatterns for fill order details and send order

--- shop/urls.py
+++ shop/urls.py
@@ -8,5 +8,7 @@ from .views import HomePageView
 urlpatterns = [
     # path('', views.show_main_page, name='main_page'),
     path('', HomePageView.as_view(), name='home'),
-    path('cake', views.make_cake_page, name='make_cake_page')
+    path('cake', views.make_cake_page, name='make_cake_page'),
+    path('order_details', views.order_details, name='order_details'),
+    path('make_order', views.make_order, name='make_order')
 ]
