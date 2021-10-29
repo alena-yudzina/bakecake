127a16005b9433b674950a03fa21416ecc7d891c
Stas Garanzha <garanzha.s.a@yandex.ru>
2021-10-29 10:15:15 +0300

    add check promocode logic and saving order logic

--- shop/views.py
+++ shop/views.py
@@ -4,14 +4,17 @@ from django.http import JsonResponse
 from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
 
-from shop.models import Berry, CakeLevel, CakeForm, Decor, Topping
+from shop.models import Berry, Cake, CakeLevel, CakeForm, Decor, Order, Topping, PromoCode
 
 from .forms import CakeConstructorForm, OrderDetailsForm
 
 
-def check_promo_code(request, code):
-    code_is_used = request.user.orders.filter(promo_code__code=code).exists()
-    return JsonResponse({'codeIsUsed': code_is_used})
+def get_and_check_promo_code(request):
+    actualPromoCode = PromoCode.objects.first().code
+    code_is_used = request.user.orders.filter(promo_code__code=actualPromoCode).exists()
+    return JsonResponse(
+        {'actualCode': actualPromoCode,'thisClientUsed': code_is_used}
+    )
 
 
 def show_main_page(request):
@@ -33,19 +36,48 @@ def order_details(request):
         return redirect(urls.reverse('make_cake_page'))
     cake_params = CakeConstructorForm(request.POST)
     cake_params.is_valid()
-    cake_level_price = CakeLevel.objects.get(pk=cake_params.cleaned_data['cake_level']).price
-    cake_form_price = CakeForm.objects.get(pk=cake_params.cleaned_data['cake_form']).price
-    topping_price = Topping.objects.filter(pk__in=cake_params.cleaned_data['topping']).values_list('price', flat=True)
-    berry_price = Berry.objects.filter(pk__in=cake_params.cleaned_data['berry']).values_list('price', flat=True)
-    decor_price = Decor.objects.filter(pk__in=cake_params.cleaned_data['decor']).values_list('price', flat=True)
-    total_price = sum([cake_level_price, cake_form_price, *topping_price, *berry_price, *decor_price])
-    form = OrderDetailsForm(initial={'price': total_price})
-    return render(request, 'order_details.html', {'form': form})
+    total_price = sum([
+        CakeLevel.objects.get(pk=cake_params.cleaned_data['cake_level']).price,
+        CakeForm.objects.get(pk=cake_params.cleaned_data['cake_form']).price,
+        Topping.objects.get(pk__in=cake_params.cleaned_data['topping']).price,
+        *Decor.objects.filter(pk__in=cake_params.cleaned_data['decor']).values_list('price', flat=True),
+        *Berry.objects.filter(pk__in=cake_params.cleaned_data['berry']).values_list('price', flat=True)
+    ])
+    form = OrderDetailsForm(initial={'price': total_price,})
+    return render(request, 'order_details.html', {'form': form, 'cake_params': cake_params, 'price': total_price})
 
 
 def make_order(request):
     if request.method == 'GET':
         return redirect(urls.reverse('make_cake_page'))
+    order_details = OrderDetailsForm(request.POST)
+    order_details.is_valid()
+    order_details = order_details.cleaned_data
+    cake_constructor_form = CakeConstructorForm(request.POST)
+    cake_constructor_form.is_valid()
+    cake_params = cake_constructor_form.cleaned_data
+    cake = Cake.objects.create(
+        level=CakeLevel.objects.get(pk=cake_params['cake_level']),
+        form=CakeForm.objects.get(pk=cake_params['cake_form']),
+        topping=Topping.objects.get(pk=cake_params['topping']),
+        caption_on_cake=cake_params['caption_on_cake']
+    )
+    cake.decor.add(*map(lambda pk: Decor.objects.get(pk=pk), cake_params['decor']))
+    cake.berry.add(*map(lambda pk: Berry.objects.get(pk=pk), cake_params['berry']))
+    # собираем заказ
+    if request.POST.get('promo_code'):
+        promo_code = PromoCode.objects.get(code=request.POST.get('promo_code'))
+    else:
+        promo_code = None
+    Order.objects.create(
+        client=request.user,
+        cake=cake,
+        destination=order_details['destination'],
+        delivery_time=order_details['order_datetime'],
+        promo_code=promo_code,
+        total_price=request.POST.get('cake_price')
+    )
+    # Докрутить - нужно редиректеть на страницу "Мои заказы"
     return render(request, 'order_details.html', {})
 
 
